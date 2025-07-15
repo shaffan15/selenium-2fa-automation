afc31724426f4319508000b41fc1c16b3dab26f4
Shaffan Shaikh <shaffan15@gmail.com>
2025-07-16 00:16:08 +0530

    Add files via upload

---
+++ src/main/java/totp/TOTPGenerator.java
@@ -0,0 +1,11 @@
+package com.learning.helper;
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+
+public class TOTPGenerator {
+
+	public static String getTotp(String secretKey) {
+		GoogleAuthenticator gAuth = new GoogleAuthenticator();
+		return String.valueOf(gAuth.getTotpPassword(secretKey));
+	}
+}
